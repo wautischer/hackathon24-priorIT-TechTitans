53e88f5c47a7b5cd3828f82133a70ab6d04e2e93
Laurin Wautischer <wautischer@outlook.com>
2024-06-28 15:53:39 +0200

    update persistence mapping

--- Feedback/src/main/java/at/techtitans/hackathon/apps/TestApp.java
+++ Feedback/src/main/java/at/techtitans/hackathon/apps/TestApp.java
@@ -1,6 +1,7 @@
 package at.techtitans.hackathon.apps;
 
 import at.techtitans.hackathon.entities.Employee;
+import at.techtitans.hackathon.entities.UserFeedback;
 import at.techtitans.hackathon.persistence.FeedbackDAO;
 
 import java.util.List;
@@ -13,5 +14,10 @@ public class TestApp {
         }
 
         System.out.println(FeedbackDAO.checkPassword("password1","tom_john"));
+
+        List<UserFeedback> temp = FeedbackDAO.getRatingsByEmployeeID(85);
+        for (UserFeedback userFeedback : temp) {
+            System.out.println(userFeedback);
+        }
     }
 }
